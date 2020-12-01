65dbd6201b144871ad9d2197ea59a92f045a2794
kevin-grylls <eilgwon88@gmail.com>
2020-12-01 16:57:35 +0900

    update code

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,15 @@
 module.exports = {
-  verbose: true,
-  testEnvironment: "jsdom",
-  testRegex: "test/.*\\.(test|spec)?\\.(js|ts)$",
-  moduleFileExtensions: ["ts", "js", "json", "node"],
+  moduleFileExtensions: ["js", "json", "jsx", "ts", "tsx", "json"],
+  transform: {
+    "^.+\\.(js|jsx)?$": "babel-jest",
+  },
+  testEnvironment: "node",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+  testMatch: [
+    "<rootDir>/**/*.test.(js|jsx|ts|tsx)",
+    "<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))",
+  ],
+  transformIgnorePatterns: ["<rootDir>/node_modules/"],
 };
